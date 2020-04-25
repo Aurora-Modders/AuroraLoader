21fa19ce43864efdd0cc7d77e3da6858dbbd83aa
01010100b <caveman1917@hotmail.com>
2020-04-25 10:08:33 +0200

    aurora installer/updater

---
+++ AuroraLoader/Installer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace AuroraLoader
+{
+    class Installer
+    {
+        public static void UpdateAurora(GameInstallation current, GameVersion update, Dictionary<string, string> aurora_files)
+        {
+            if (current.InstalledVersion.Version.Major == update.Version.Major)
+            {
+                aurora_files.Remove("Major");
+            }
+            if (current.InstalledVersion.Version.Minor == update.Version.Minor)
+            {
+                aurora_files.Remove("Minor");
+            }
+            if (current.InstalledVersion.Version.Patch == update.Version.Patch)
+            {
+                aurora_files.Remove("Patch");
+                aurora_files.Remove("Rev"); // deprecated
+            }
+
+            foreach (var piece in aurora_files.Keys.ToList())
+            {
+                if (!piece.Equals("Major") && !piece.Equals("Minor") && !piece.Equals("Patch") && !piece.Equals("Rev"))
+                {
+                    aurora_files.Remove(piece);
+                }
+            }
+
+            if (aurora_files.Count > 0)
+            {
+                var folder = Path.GetDirectoryName(current.ExecutableLocation);
+                DownloadAuroraPieces(folder, aurora_files);
+            }
+        }
+
+        public static void DownloadAuroraPieces(string folder, Dictionary<string, string> aurora_files)
+        {
+            var zip = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            var extract_folder = Path.Combine(Path.GetTempPath(), "aurora_installs");
+
+            if (Directory.Exists(extract_folder))
+            {
+                Directory.Delete(extract_folder, true);
+            }
+            Directory.CreateDirectory(extract_folder);
+
+            using (var client = new WebClient())
+            {
+                var pieces = aurora_files.Keys.ToList();
+                pieces.Sort(); // Major before Minor before Patch/Rev
+
+                foreach (var piece in pieces)
+                {
+                    if (File.Exists(zip))
+                    {
+                        File.Delete(zip);
+                    }
+
+                    client.DownloadFile(aurora_files[piece], zip);
+                    // unzipping straight into the folder might not work, 
+                    ZipFile.ExtractToDirectory(zip, extract_folder);
+
+                    foreach (var file in Directory.EnumerateFiles(extract_folder))
+                    {
+                        var dest = Path.Combine(folder, Path.GetFileName(file));
+
+                        if (File.Exists(dest))
+                        {
+                            File.Delete(dest);
+                        }
+
+                        File.Copy(file, dest);
+                    }
+                }
+            }
+
+            File.Delete(zip);
+            Directory.Delete(extract_folder, true);
+        }
+    }
+}
