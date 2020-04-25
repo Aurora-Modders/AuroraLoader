cbd1db2f93b26c8e6824c921216510dbbc96cf29
01010100b <caveman1917@hotmail.com>
2020-04-25 14:24:58 +0200

    show a progress form while downloading

---
+++ AuroraLoader/FormProgress.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AuroraLoader
+{
+    partial class FormProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.LabelWorking = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // LabelWorking
+            // 
+            this.LabelWorking.AutoSize = true;
+            this.LabelWorking.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LabelWorking.Location = new System.Drawing.Point(88, 71);
+            this.LabelWorking.Name = "LabelWorking";
+            this.LabelWorking.Size = new System.Drawing.Size(95, 30);
+            this.LabelWorking.TabIndex = 0;
+            this.LabelWorking.Text = "Working.";
+            this.LabelWorking.UseWaitCursor = true;
+            // 
+            // FormProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 390);
+            this.ControlBox = false;
+            this.Controls.Add(this.LabelWorking);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormProgress";
+            this.Text = "FormProgress";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.FormProgress_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label LabelWorking;
+    }
+}
---
+++ AuroraLoader/FormProgress.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AuroraLoader
+{
+    public partial class FormProgress : Form
+    {
+        private readonly Thread Thread;
+        public FormProgress(Thread thread)
+        {
+            InitializeComponent();
+            Thread = thread;
+        }
+
+        private void FormProgress_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (Thread.IsAlive)
+            {
+                Invoke((MethodInvoker)delegate
+                {
+                    var num = LabelWorking.Text.Count(c => c.Equals('.'));
+                    num++;
+                    if (num >= 5)
+                    {
+                        num = 1;
+                    }
+
+                    var text = "Working";
+                    for (int i = 0; i < num; i++)
+                    {
+                        text += ".";
+                    }
+
+                    LabelWorking.Text = text;
+                });
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+}
