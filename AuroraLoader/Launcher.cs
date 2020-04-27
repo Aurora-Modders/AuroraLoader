ed66f06b683f094db33b6c361632e47ae3bd00b4
01010100b <caveman1917@hotmail.com>
2020-04-27 12:21:51 +0200

    Db mods (#19)
    * bugfix open url on .net core
    
    * mod configuration takes shell command
    
    * fix exception on unknown aurora version
    
    * support for db mods
    
    * add example db mod
    
    * add a table to the db to indicate a modded save
    
    * properly install/uninstall db mods
    
    * bugfix db mod selection
    
    * ui
    
    * few people have a markdown parser installed
    
    * readme open browser failsafe
    
    * add logging
    
    * m

--- AuroraLoader/Launcher.cs
+++ AuroraLoader/Launcher.cs
@@ -1,55 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using AuroraLoader.Mods;
+using AuroraLoader.Registry;
 
 namespace AuroraLoader
 {
     static class Launcher
     {
-        public static Process Launch(IList<Mod> mods, Mod executableMod = null)
+        public static string GetConnectionString(GameInstallation installation)
+        {
+            var db = Path.Combine(installation.InstallationPath, "AuroraDB.db");
+
+            return $"Data Source={db};Version=3;New=False;Compress=True;";
+        }
+
+        public static List<Process> Launch(GameInstallation installation, ModRegistry registry, IList<Mod> mods, Mod executableMod = null)
         {
             if (mods.Any(mod => mod.Type == ModType.EXE))
             {
                 throw new Exception("Use the other parameter");
             }
 
+            UninstallDbMods(installation, registry);
+
+            var processes = new List<Process>();
+
             foreach (var mod in mods.Where(mod => mod.Type == ModType.ROOTUTILITY))
             {
                 Log.Debug("Root Utility: " + mod.Name);
-                CopyToRoot(mod);
-                Run(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), mod.Installation.ExecuteCommand);
+                CopyToFolder(mod, installation.InstallationPath);
+                var process = Run(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), mod.Installation.ExecuteCommand);
+                processes.Add(process);
             }
             foreach (var mod in mods.Where(mod => mod.Type == ModType.UTILITY))
             {
                 Log.Debug("Utility: " + mod.Name);
-                Run(mod.Installation.ModFolder, mod.Installation.ExecuteCommand);
+                var process = Run(mod.Installation.ModFolder, mod.Installation.ExecuteCommand);
+                processes.Add(process);
             }
             foreach (var mod in mods.Where(mod => mod.Type == ModType.DATABASE))
             {
                 Log.Debug("Database: " + mod.Name);
-                throw new Exception("Database mods not supported yet: " + mod.Name);
+                InstallDbMod(mod, installation);
             }
 
             if (executableMod != null)
             {
-                CopyToRoot(executableMod);
-                return Run(Program.AuroraLoaderExecutableDirectory, executableMod.Installation.ExecuteCommand);
+                CopyToFolder(executableMod, installation.InstallationPath);
+                var process = Run(Program.AuroraLoaderExecutableDirectory, executableMod.Installation.ExecuteCommand);
+                processes.Insert(0, process);
             }
             else
             {
-                return Run(Program.AuroraLoaderExecutableDirectory, "Aurora.exe");
+                var process = Run(installation.InstallationPath, "Aurora.exe");
+                processes.Insert(0, process);
             }
+
+            return processes;
         }
 
-        private static void CopyToRoot(Mod mod)
+        private static void CopyToFolder(Mod mod, string folder)
         {
             var dir = Path.GetDirectoryName(mod.Installation.ModFolder);
             foreach (var file in Directory.EnumerateFiles(dir, "*.*", SearchOption.AllDirectories).Where(f => !Path.GetFileName(f).Equals("mod.ini")))
             {
-                File.Copy(file, Path.Combine(Program.AuroraLoaderExecutableDirectory, Path.GetFileName(file)), true);
+                File.Copy(file, Path.Combine(folder, Path.GetFileName(file)), true);
             }
         }
 
@@ -83,6 +102,102 @@ namespace AuroraLoader
 
             var process = Process.Start(info);
             return process;
+        }
+
+        private static void InstallDbMod(Mod mod, GameInstallation installation)
+        {
+            const string TABLE = "CREATE TABLE IF NOT EXISTS A_THIS_SAVE_IS_MODDED (ModName Text PRIMARY KEY);";
+
+            using (var connection = new SQLiteConnection(GetConnectionString(installation)))
+            {
+                connection.Open();
+                var table = new SQLiteCommand(TABLE, connection);
+                table.ExecuteNonQuery();
+
+                var files = Directory.EnumerateFiles(mod.Installation.ModFolder, "*.sql").ToList();
+                try
+                {
+                    var uninstall = files.Single(f => Path.GetFileName(f).Equals("uninstall.sql"));
+                    files.Remove(uninstall);
+                }
+                catch (Exception)
+                {
+                    Log.Debug($"No uninstall for db mod: {mod.Name}");
+                }
+                
+                foreach (var file in files)
+                {
+                    var sql = File.ReadAllText(file);
+                    var command = new SQLiteCommand(sql, connection);
+                    command.ExecuteNonQuery();
+
+                    sql = $"INSERT INTO A_THIS_SAVE_IS_MODDED(ModName)" +
+                            $"SELECT '{mod.Name}'" +
+                            $"WHERE NOT EXISTS(SELECT 1 FROM A_THIS_SAVE_IS_MODDED WHERE ModName = '{mod.Name}');";
+
+                    command = new SQLiteCommand(sql, connection);
+                    command.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+        }
+
+        private static void UninstallDbMods(GameInstallation installation, ModRegistry registry)
+        {
+            var installed = registry.Mods.Where(m => m.Installed && m.Type == ModType.DATABASE).ToList();
+
+            using (var connection = new SQLiteConnection(GetConnectionString(installation)))
+            {
+                connection.Open();
+
+                var sql = "SELECT * FROM sqlite_master WHERE name ='A_THIS_SAVE_IS_MODDED' and type='table';";
+                var command = new SQLiteCommand(sql, connection);
+                var reader = command.ExecuteReader();
+                if (!reader.Read())
+                {
+                    reader.Close();
+                    connection.Close();
+                    return;
+                }
+                else
+                {
+                    reader.Close();
+                }
+
+                sql = "SELECT * FROM A_THIS_SAVE_IS_MODDED";
+                command = new SQLiteCommand(sql, connection);
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var name = reader[0].ToString();
+                    var mod = installed.FirstOrDefault(m => m.Name.Equals(name));
+
+                    if (mod == null)
+                    {
+                        throw new Exception($"Installed db mod {name} not found");
+                    }
+                    else
+                    {
+                        var file = Path.Combine(mod.Installation.ModFolder, "uninstall.sql");
+                        if (!File.Exists(file))
+                        {
+                            throw new Exception($"Db mod {name} uninstall.sql not found");
+                        }
+                        
+                        sql = File.ReadAllText(file);
+                        sql += $"\nDELETE FROM A_THIS_SAVE_IS_MODDED\nWHERE ModName = '{name}';";
+                        command = new SQLiteCommand(sql, connection);
+                        command.ExecuteNonQuery();
+
+                        Log.Debug($"Uninstalled db mod: {name}");
+                    }
+                }
+
+                reader.Close();
+                connection.Close();
+            }
         }
     }
 }
