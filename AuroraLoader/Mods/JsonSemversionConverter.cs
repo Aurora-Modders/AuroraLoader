8256bd12c4986898008d9be7180b58b513617f29
thashepherd <matthew.j.alioto@gmail.com>
2020-04-29 22:58:07 -0400

    Add JsonSemversionConverter

---
+++ AuroraLoader/Mods/JsonSemversionConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Semver;
+
+namespace AuroraLoader.Mods
+{
+    public class JsonSemversionConverter : JsonConverter<SemVersion>
+    {
+        public override SemVersion Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => SemVersion.Parse(reader.GetString(), false);
+
+        public override void Write(Utf8JsonWriter writer, SemVersion value, JsonSerializerOptions options) => value.ToString();
+    }
+}
